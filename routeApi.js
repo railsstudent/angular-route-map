ec4cb5cf6b44d904186d27af49d23a7abe55fedd
Connie Leung <cleung334@gmail.com>
2015-05-26 00:06:07 +0800

    Move routers to separate js file.
    Signed-off-by: Connie Leung <cleung334@gmail.com>

---
+++ routeApi.js
@@ -0,0 +1,122 @@
+
+var express = require("express");
+var fs = require('fs');
+
+
+// API Documentation: http://expressjs.com/4x/api.html#router
+// http://scotch.io/tutorials/javascript/learn-to-use-the-new-router-in-expressjs-4
+// http://scotch.io/tutorials/javascript/build-a-restful-api-using-node-and-express-4
+// ROUTES FOR OUR API
+// =============================================================================
+// http://codereview.stackexchange.com/questions/51614/exporting-routes-in-node-js-express-4
+module.exports = (function () {
+	'use strict';
+	var router = express.Router();
+
+	router.use(function(req, res, next) {
+		// do logging
+		console.log('%s %s %s', req.method, req.url, req.path);
+		next(); // make sure we go to the next routes and don't stop here
+	});
+
+
+	var sendJson = function _readFile(filename, req, res) {
+		fs.readFile(filename, 'utf8', function (err, data) {
+			if (err) {
+				console.log('Error: ' + err);
+			    return;
+			}
+			var jsondata = JSON.parse(data);
+			res.send(jsondata);
+		});
+	};
+
+	var sendOneRoute = function _readFile(filename, req, res) {
+		fs.readFile(filename, 'utf8', function (err, data) {
+			if (err) {
+				console.log('Error: ' + err);
+			    return;
+			}
+			var jsonData = JSON.parse(data);
+			var routes = jsonData.routes;
+			var arr = undefined;
+			var intId = parseInt(req.params.id);
+			for (var i = 0; i < routes.length; i++) {
+				if (routes[i].id === intId) {
+					arr = routes[i];
+				}
+			}
+			res.send(arr);
+		});
+	};
+
+	router.route('/route/morning')
+		// get all the morning routes (accessed at GET http://localhost:5000/api/v1/route/morning)
+		.get(function(req, res) {
+			var file = __dirname + '/dist/api/v1/morning_routes.json';
+			sendJson(file, req, res);
+		});
+
+	router.route('/route/morning/:id')
+		// get all the morning routes (accessed at
+		// GET http://localhost:5000/api/v1/route/morning/:id)
+		.get(function(req, res) {
+			var file = __dirname + '/dist/api/v1/morning_routes.json';
+			sendOneRoute(file, req, res);
+		});
+
+	router.route('/route/names/:shift')
+		// get all the morning routes 
+		// (accessed at GET http://localhost:5000/api/v1/route/names/:shift)
+		.get(function(req, res) {
+
+			var file = __dirname + '/dist/api/v1/' + req.params.shift + '_routes.json';
+			fs.readFile(file, 'utf8', function (err, data) {
+				if (err) {
+					console.log('Error: ' + err);
+				    return;
+				}
+	  			var data = JSON.parse(data);
+	  			var routes = data.routes;
+	  			var arr = [];
+	  			for (var i = 0; i < routes.length; i++) {
+	  				arr.push({"id" : routes[i].id, "name" : routes[i].name});
+	  			}
+				res.send({ "routes" : arr });
+			});
+		});
+
+	// get all the evening routes (accessed at GET http://localhost:5000/api/v1/route/evening)
+	router.route('/route/evening')
+		.get(function(req, res) {
+			var file = __dirname + '/dist/api/v1/evening_routes.json';
+			sendJson(file, req, res);
+		});
+
+	router.route('/route/evening/:id')
+		.get(function(req, res) {
+			var file = __dirname + '/dist/api/v1/evening_routes.json';
+			sendOneRoute(file, req, res);
+		});
+
+	// home page route (http://localhost:5000)
+	/*router.get('/', function(req, res) {
+		res.sendfile('index.html');
+	});*/
+
+
+	router.route('/route/meeting')
+		// get all the morning routes (accessed at GET http://localhost:5000/api/v1/route/meeting)
+		.get(function(req, res) {
+			var file = __dirname + '/dist/api/v1/meeting_routes.json';
+			sendJson(file, req, res);
+	});
+
+	router.route('/route/meeting/:id')
+		.get(function(req, res) {
+			var file = __dirname + '/dist/api/v1/meeting_routes.json';
+			sendOneRoute(file, req, res);
+		});
+
+	return router;
+})();
--- web.js
+++ web.js
@@ -1,163 +1,32 @@
+'use strict';
+
 var compression = require('compression');
 var express = require("express");
 var logfmt = require("logfmt");
-var fs = require('fs');
 var app = express();
+var router = require('./routeApi');   // move routes to routeApi.js
 
 app.use(logfmt.requestLogger());
-//app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 app.use(compression());
 
-// API Documentation: http://expressjs.com/4x/api.html#router
-// http://scotch.io/tutorials/javascript/learn-to-use-the-new-router-in-expressjs-4
-// http://scotch.io/tutorials/javascript/build-a-restful-api-using-node-and-express-4
-// ROUTES FOR OUR API
-// =============================================================================
-var router = express.Router(); 				// get an instance of the express Router
-
-// always invoked
-// middleware to use for all requests
-router.use(function(req, res, next) {
-	// do logging
-	// console.log('Something is happening.');
-	console.log('%s %s %s', req.method, req.url, req.path);
-	next(); // make sure we go to the next routes and don't stop here
-});
-
-var sendJson = function _readFile(filename, req, res) {
-	fs.readFile(filename, 'utf8', function (err, data) {
-		if (err) {
-			console.log('Error: ' + err);
-		    return;
-		}
-		var jsondata = JSON.parse(data);
-		res.send(jsondata);
-	});
-};
-
-var sendOneRoute = function _readFile(filename, req, res) {
-	fs.readFile(filename, 'utf8', function (err, data) {
-		if (err) {
-			console.log('Error: ' + err);
-		    return;
-		}
-		var jsonData = JSON.parse(data);
-		var routes = jsonData.routes;
-		var arr = undefined;
-		var intId = parseInt(req.params.id);
-		for (var i = 0; i < routes.length; i++) {
-			if (routes[i].id === intId) {
-				arr = routes[i];
-			}
-		}
-		res.send(arr);
-	});
-};
-
-router.route('/api/v1/route/morning')
-	// get all the morning routes (accessed at GET http://localhost:5000/api/v1/route/morning)
-	.get(function(req, res) {
-//		var file = __dirname + '/app/api/v1/morning_routes.json';
-		var file = __dirname + '/dist/api/v1/morning_routes.json';
-		sendJson(file, req, res);
-	});
-
-router.route('/api/v1/route/morning/:id')
-	// get all the morning routes (accessed at
-	// GET http://localhost:5000/api/v1/route/morning/:id)
-	.get(function(req, res) {
-//		var file = __dirname + '/app/api/v1/morning_routes.json';
-		var file = __dirname + '/dist/api/v1/morning_routes.json';
-		sendOneRoute(file, req, res);
-	});
-
-router.route('/api/v1/route/names/:shift')
-	// get all the morning routes 
-	// (accessed at GET http://localhost:5000/api/v1/route/names/:shift)
-	.get(function(req, res) {
-
-//		var file = __dirname + '/app/api/v1/' + req.params.shift + '_routes.json';
-		var file = __dirname + '/dist/api/v1/' + req.params.shift + '_routes.json';
-		fs.readFile(file, 'utf8', function (err, data) {
-			if (err) {
-				console.log('Error: ' + err);
-			    return;
-			}
-  			var data = JSON.parse(data);
-  			var routes = data.routes;
-  			var arr = [];
-  			for (var i = 0; i < routes.length; i++) {
-  				arr.push({"id" : routes[i].id, "name" : routes[i].name});
-  			}
-			res.send({ "routes" : arr });
-		});
-	});
-
-// get all the evening routes (accessed at GET http://localhost:5000/api/v1/route/evening)
-router.route('/api/v1/route/evening')
-	.get(function(req, res) {
-//		var file = __dirname + '/app/api/v1/evening_routes.json';
-		var file = __dirname + '/dist/api/v1/evening_routes.json';
-		sendJson(file, req, res);
-	});
-
-router.route('/api/v1/route/evening/:id')
-	.get(function(req, res) {
-//		var file = __dirname + '/app/api/v1/evening_routes.json';
-		var file = __dirname + '/dist/api/v1/evening_routes.json';
-		sendOneRoute(file, req, res);
-	});
-
-// home page route (http://localhost:5000)
-router.get('/', function(req, res) {
-//	res.sendfile('./app/index.html');
+var home = express.Router(); 
+home.get("/", function (req, res) {
 	res.sendfile('index.html');
 });
 
-
-router.route('/api/v1/route/meeting')
-	// get all the morning routes (accessed at GET http://localhost:5000/api/v1/route/meeting)
-	.get(function(req, res) {
-//		var file = __dirname + '/app/api/v1/meeting_routes.json';
-		var file = __dirname + '/dist/api/v1/meeting_routes.json';
-		sendJson(file, req, res);
-});
-
-router.route('/api/v1/route/meeting/:id')
-	.get(function(req, res) {
-//		var file = __dirname + '/app/api/v1/meeting_routes.json';
-		var file = __dirname + '/dist/api/v1/meeting_routes.json';
-		sendOneRoute(file, req, res);
-	});
-
 // http://stackoverflow.com/questions/23860275/javascript-angular-not-loading-when-using-express
 //add this so the browser can GET the bower files
-//app.use('/bower_components', express.static(__dirname + '/bower_components'));
-//app.use(express.static(__dirname + '/app'));
 app.use(express.static(__dirname + '/dist'));
 
-// http://stackoverflow.com/questions/19687667/making-ajax-call-angular-to-node-js-express-js
-/*var allowCrossDomain = function (req, res, next) {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-	res.header('Access-Control-Allow-Headers', 'content-type, Authorization, Content-Length, X-Requested-With, Origin, Accept');
-
-	if ('OPTIONS' === req.method) {
-	    res.send(200);
-	} else {
-	    next();
-	}
-};*/
-
-//app.use(allowCrossDomain);
-
 // apply the routes to our application
-app.use('/', router);
+// app.use('/', router);
+app.use('/api/v1', router);
+app.use('/', home);
 
 // Error-handling middleware 
 app.use(function(err, req, res, next){
   console.error(err.stack);
-  res.send(500, 'Something broke!');
+  res.send(500, 'Something broke!'  + err.message);
 });
 
 var port = Number(process.env.PORT || 5000);

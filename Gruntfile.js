de6d593ddad094ea367c86c03fa99eadeb7339d2
Connie <cleung334@gmail.com>
2015-10-17 21:32:32 +0800

    replace ,* with **
    Signed-off-by: Connie <cleung334@gmail.com>

--- Gruntfile.js
+++ Gruntfile.js
@@ -362,7 +362,7 @@ module.exports = function (grunt) {
         expand: true,
         cwd: '<%= yeoman.app %>/styles',
         dest: '.tmp/styles/',
-        src: '*.css'
+        src: '{**/}*.css'
       }
     },
 
